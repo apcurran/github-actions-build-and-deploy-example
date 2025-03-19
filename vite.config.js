9308a743c51e067032cbb8472bf6b3401586a56b
Alex Curran <alexcurran.va@gmail.com>
2025-03-19 15:35:26 -0500

    Adjust base URL path for prod app

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+// get intellisense in IDE
+/** @type {import('vite').UserConfig} */
+export default {
+    // production server resolves to https://<my-example.com>/github-actions-build-and-deploy-example
+    // not https://<my-example.com>
+    base: "/github-actions-build-and-deploy-example/"
+}
